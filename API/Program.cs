9176006aa82fe26ef22aa9430094a788c2804396
Biswajit-Panda <bp26012000@gmail.com>
2023-08-31 00:01:42 +0530

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<StoreContext>(opt => {
 });
 
 var app = builder.Build();
+builder.Services.AddCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,6 +23,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(opt => 
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 
